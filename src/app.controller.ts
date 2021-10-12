dcdce865e81958e92db6245f31d2b99dd0e6beff
konstantin <konstantin.karchevskiy@devbrother.com>
2021-10-12 18:00:21 +0300

    feat: change app result to reasonable object

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return {
+      title: 'Git practices',
+      version: '0.0.0',
+      author: 'KK',
+    };
   }
 }
